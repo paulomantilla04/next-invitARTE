9f529d7b6a8ad33c3d06e1a1b8185be91b0f1cb7
unknown <ma475671@uaeh.edu.mx>
2024-05-18 10:56:29 -0600

    first commit

---
+++ src/app/table/page.tsx
@@ -0,0 +1,8 @@
+export default function Table() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold">Table</h1>
+            <p className="text-lg">This is a table page</p>
+        </div>
+    );
+}
